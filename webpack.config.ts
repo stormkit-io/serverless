38fa6bbf2f5b3182981a8c9ec152cc99193c8afa
Savas Vedova <savas@stormkit.io>
2022-07-02 10:39:05 +0300

    feat: add entries configuration
    - Rename index.ts to serverless.ts

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,9 +1,26 @@
-import { Configuration } from "webpack";
+import { Configuration, ModuleOptions, ResolveOptions } from "webpack";
 import path from "path";
+
+const resolve: ResolveOptions = {
+  extensions: [".tsx", ".ts", ".js"],
+  alias: {
+    "~": path.join(__dirname, "src"),
+  },
+};
+
+const module: ModuleOptions = {
+  rules: [
+    {
+      test: /\.tsx?$/,
+      use: "ts-loader",
+      exclude: /node_modules/,
+    },
+  ],
+};
 
 const config: Configuration = {
   entry: {
-    index: "./src/index.ts",
+    index: "./src/serverless.ts",
     "dev-server": "./src/dev-server/index.ts",
   },
   output: {
@@ -13,24 +30,32 @@ const config: Configuration = {
   },
   mode: "production",
   target: "node",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-    alias: {
-      "~": path.join(__dirname, "src"),
-    },
-  },
+  resolve,
+  module,
   optimization: {
     minimize: false,
   },
 };
 
-export default config;
+const entries: Configuration = {
+  entry: {
+    "nuxt-v2": "./src/entries/nuxt/v2/server.ts",
+    next: "./src/entries/next/server.ts",
+    default: "./src/entries/default/server.ts",
+    angular: "./src/entries/angular/server.ts",
+  },
+  output: {
+    filename: "entries/[name].js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "commonjs",
+  },
+  mode: "production",
+  target: "node",
+  resolve,
+  module,
+  optimization: {
+    minimize: false,
+  },
+};
+
+export default [config, entries];
