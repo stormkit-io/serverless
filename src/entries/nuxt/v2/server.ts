cf0265c9f9f5772e33635532a01fde6c5fe85c2d
Savas Vedova <savas@stormkit.io>
2022-07-04 08:45:14 +0300

    chore: use load nuxt function

--- src/entries/nuxt/v2/server.ts
+++ src/entries/nuxt/v2/server.ts
@@ -1,34 +1,29 @@
 import http from "http";
-import serverless from "~/serverless";
+import serverless from "../../../serverless";
+import { load } from "../../../utils";
 
 // This is the lambda file that will be compiled on build time.
 // It will be used as the entry point for the lambda function.
 process.env.NUXT_TELEMETRY_DISABLED = "1";
 
-const load =
-  typeof __non_webpack_require__ !== "undefined"
-    ? __non_webpack_require__
-    : require;
-
-const { Nuxt } = load("nuxt-start");
-
-const nuxt = new Nuxt({
-  dev: false,
-  render: {
-    compressor: false,
-    http2: false,
-  },
-});
-
-let ready: boolean;
+const { loadNuxt } = load("nuxt");
+let nuxt: any;
 
 export const renderer = serverless(
   async (
     req: http.IncomingMessage,
     res: http.ServerResponse
   ): Promise<void> => {
-    if (!ready) {
-      ready = await nuxt.ready();
+    if (!nuxt) {
+      nuxt = await loadNuxt({
+        for: "start",
+        configOverrides: {
+          render: {
+            compressor: false,
+            http2: false,
+          },
+        },
+      });
     }
 
     return new Promise((resolve) => {
