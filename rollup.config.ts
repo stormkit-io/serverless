c6675f64204463074d70e0c12e222d2770d738cc
Savas Vedova <savas@stormkit.io>
2022-09-26 15:48:51 +0300

    fix: rollup config
    Previously the generated entry files were not standalone because they
    were sharing the same rollup config. Create a separate config entry
    for each entry file so that builds are always standalone.

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,25 +4,27 @@ import commonjs from "@rollup/plugin-commonjs";
 import jsonResolve from "@rollup/plugin-json";
 import dts from "rollup-plugin-dts";
 
+const inputs = {
+  serverless: "./src/serverless.ts",
+  presets: "./src/presets/presets.ts",
+  "dev-server": "./src/dev-server/dev-server.ts",
+  "entries/nuxt-v2": "./src/entries/nuxt/server-v2.ts",
+  "entries/next": "./src/entries/next/server.ts",
+  "entries/default": "./src/entries/default/server.ts",
+  "entries/angular": "./src/entries/angular/server.ts",
+  "entries/api": "./src/entries/api/server.ts",
+};
+
 export default [
-  {
-    input: {
-      serverless: "./src/serverless.ts",
-      presets: "./src/presets/presets.ts",
-      "dev-server": "./src/dev-server/dev-server.ts",
-      "entries/nuxt-v2": "./src/entries/nuxt/server-v2.ts",
-      "entries/next": "./src/entries/next/server.ts",
-      "entries/default": "./src/entries/default/server.ts",
-      "entries/angular": "./src/entries/angular/server.ts",
-      "entries/api": "./src/entries/api/server.ts",
-    },
+  ...Object.keys(inputs).map((key) => ({
+    input: { [key]: inputs[key] },
     output: {
       dir: "dist",
       format: "cjs",
       exports: "auto",
     },
     plugins: [typescript(), commonjs(), nodeResolve(), jsonResolve()],
-  },
+  })),
   {
     input: {
       serverless: "./dist/types/serverless.d.ts",
